979454724610767ba7b303084029e1becb65985d
Lucio Andrés Illanes Albornoz (arab, vxp) <lucio@lucioillanes.de>
2017-06-07 12:28:25 +0200

    assets/CurriculumVitae.mk: call texi2pdf(1) w/ -c instead of --mostly-clean.

--- assets/CurriculumVitae.mk
+++ assets/CurriculumVitae.mk
@@ -1,11 +1,11 @@
 public::	CurriculumVitae.tex
-		texi2pdf --mostly-clean -t '\\newcommand{\\varLang}[0]{ngerman}' -t '\\newcommand{\\varPrivate}[0]{0}' $<
+		texi2pdf -c -t '\\newcommand{\\varLang}[0]{ngerman}' -t '\\newcommand{\\varPrivate}[0]{0}' $<
 		mv CurriculumVitae.pdf "Curriculum Vitae (öffentliche Version, ohne Referenzen).pdf"
-		texi2pdf  --mostly-clean-t '\\newcommand{\\varLang}[0]{british}' -t '\\newcommand{\\varPrivate}[0]{0}' $<
+		texi2pdf -c -t '\\newcommand{\\varLang}[0]{british}' -t '\\newcommand{\\varPrivate}[0]{0}' $<
 		mv CurriculumVitae.pdf "Curriculum Vitae (public version, excludes character references).pdf"
 
 private::	CurriculumVitae.tex
-		texi2pdf --mostly-clean -t '\\newcommand{\\varLang}[0]{ngerman}' -t '\\newcommand{\\varPrivate}[0]{1}' $<
+		texi2pdf -c -t '\\newcommand{\\varLang}[0]{ngerman}' -t '\\newcommand{\\varPrivate}[0]{1}' $<
 		mv CurriculumVitae.pdf "Curriculum Vitae (private Version, mit Referenzen).pdf"
-		texi2pdf --mostly-clean -t '\\newcommand{\\varLang}[0]{british}' -t '\\newcommand{\\varPrivate}[0]{1}' $<
+		texi2pdf -c -t '\\newcommand{\\varLang}[0]{british}' -t '\\newcommand{\\varPrivate}[0]{1}' $<
 		mv CurriculumVitae.pdf "Curriculum Vitae (private version, includes character references).pdf"
