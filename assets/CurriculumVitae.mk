6be7fcbc04631b4d561503d6b90ae545aaf97137
Lucio Andrés Illanes Albornoz <lucio@lucioillanes.de>
2019-02-09 12:53:46 +0100

    assets/CurriculumVitae...pdf, includes/{de,en,es}/menu.html: updated. assets/CurriculumVitae.{mk,tex}: updated. include/{de,en,es}/about.html: updates self-description. include/{de,en,es}/tarot.html: removed.

--- assets/CurriculumVitae.mk
+++ assets/CurriculumVitae.mk
@@ -1,11 +1,11 @@
 public::	CurriculumVitae.tex
 		texi2pdf -c -t '\\newcommand{\\varLang}[0]{ngerman}' -t '\\newcommand{\\varPrivate}[0]{0}' $<
-		mv CurriculumVitae.pdf "Curriculum Vitae (öffentliche Version, ohne Referenzen).pdf"
+		mv CurriculumVitae.pdf "Curriculum Vitae (Deutsch).pdf"
 		texi2pdf -c -t '\\newcommand{\\varLang}[0]{british}' -t '\\newcommand{\\varPrivate}[0]{0}' $<
-		mv CurriculumVitae.pdf "Curriculum Vitae (public version, excludes character references).pdf"
+		mv CurriculumVitae.pdf "Curriculum Vitae (English).pdf"
 
 private::	CurriculumVitae.tex
 		texi2pdf -c -t '\\newcommand{\\varLang}[0]{ngerman}' -t '\\newcommand{\\varPrivate}[0]{1}' $<
-		mv CurriculumVitae.pdf "private/Curriculum Vitae (private Version, mit Referenzen).pdf"
+		mv CurriculumVitae.pdf "Curriculum Vitae (Deutsch, private Version, mit Referenzen).pdf"
 		texi2pdf -c -t '\\newcommand{\\varLang}[0]{british}' -t '\\newcommand{\\varPrivate}[0]{1}' $<
-		mv CurriculumVitae.pdf "private/Curriculum Vitae (private version, includes character references).pdf"
+		mv CurriculumVitae.pdf "Curriculum Vitae (English, private version, includes character references).pdf"
